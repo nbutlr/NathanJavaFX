5140ff3f7d19c4534ff0c64bde5297b9850d741e
nbutlr <05257@farnborough.ac.uk>
2020-11-13 11:50:59 +0000

    a bunch of updates

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,33 +1,58 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import java.util.Optional;
 
 public class HelloWorld extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
 
         StackPane root = new StackPane();
         Scene scene = new Scene(root, 300, 250);
+        root.setStyle("-fx-background-color: darkviolet;");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn.setStyle( " -fx-background-color: lime;" +
+        btn.setStyle( " -fx-background-color: violet;" +
                 "-fx-font-size: 16;" +
-                "-fx-font-family: 'Arial Black';" +
+                "-fx-font-family: 'Comic Sans MS';" +
                 "-fx-padding: 10;" +
-                "-fx-text-fill: purple");
-        btn.setOnAction(event -> System.out.println("Hello World!"));
+                "-fx-text-fill: lavender");
+        final int[] counter = {0};
+        btn.setOnAction(event -> {
+            counter[0] +=1;
+            System.out.println("Hello World! "+ counter[0]);
+        });
         root.getChildren().add(btn);
-
         primaryStage.setScene(scene);
+        primaryStage.setOnCloseRequest(HelloWorld::exitPrompt);
         primaryStage.show();
 
     }
+    private static void exitPrompt(WindowEvent we) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Are you sure you want to exit?");
+
+        Optional result = alert.showAndWait();
+        if (result.get() == ButtonType.OK){
+            System.exit(0);
+        } else {
+            we.consume();
+        }
+
+    }
+
 }
+
+
 
